b33b1e9c6e393afb6dd7f92be45509fefa2991bb
JFernando12 <fire.jf2012@gmail.com>
2023-06-08 09:54:47 -0600

    changes

--- app/(shared)/Navbar.tsx
+++ app/(shared)/Navbar.tsx
@@ -9,7 +9,7 @@ const Navbar = (props: Props) => {
   return (
     <header className="mb-5">
       <nav className="flex justify-between items-center w-full bg-wh-900 text-wh-10 px-10 py-4">
-        <div>
+        <div className="hidden sm:block">
           <SocialLinks />
         </div>
         <div className="flex justify-between items-center gap-10">
@@ -22,16 +22,25 @@ const Navbar = (props: Props) => {
         </div>
       </nav>
       <div className="flex justify-between">
-        <div className="basis-2/3">
+        <div className="basis-full text-center xs:text-left xs:basis-2/5 ">
           <h1 className="font-bold text-3xl md:text-5xl">BLOG OF THE FUTURE</h1>
           <p className="text-sm mt-3">
             Blog dedicated towards AI and generations and job automation
           </p>
         </div>
-        <div className="basis-full relative w-auto h-32">
-          <Image alt="advert-1" src={Ad1} />
+        <div className="hidden xs:block relative basis-3/5 w-auto h-32">
+          <Image
+            fill
+            src={Ad1}
+            sizes="(max-width: 480px) 100vw"
+            alt="ad"
+            style={{
+              objectFit: 'cover',
+            }}
+          />
         </div>
       </div>
+      <hr className="border-1 mx-10" />
     </header>
   );
 };
