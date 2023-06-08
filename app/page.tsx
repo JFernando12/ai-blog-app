ebc10109d3f30523011b7b5a1ac7787c01a635f5
JFernando12 <fire.jf2012@gmail.com>
2023-06-08 14:26:24 -0600

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
 import Image from 'next/image';
+import { prisma } from './api/clients';
 
-const getPosts = async () => {};
+const getPosts = async () => {
+  const posts = await prisma.post.findMany();
+  console.log('posts', posts);
+};
 
-export default function Home() {
+export default async function Home() {
+  const posts = await getPosts();
   return <main className="px-10 leading-7"></main>;
 }
